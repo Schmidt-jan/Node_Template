8547e1dc439aa341199bb89f16f92a08ae9c7363
jan <Schmidt_Jan@outlook.com>
2021-08-01 13:13:05 +0200

    setup server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Project created")
+import express from "express"
+import morgan from "morgan"
+
+const port: number = 4000
+const app = express()
+
+app.use(morgan("dev"))
+
+app.get("/", (req, res) => {
+    res.send("Dispatcher")
+})
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
+})
