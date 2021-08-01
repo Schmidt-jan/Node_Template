6216f6b8e156c364b3c3975e245e121350b2c650
jan <Schmidt_Jan@outlook.com>
2021-08-01 14:50:23 +0200

    create documentation with swagger when started

---
+++ src/routes/ticket_routes.ts
@@ -0,0 +1,47 @@
+import express from "express"
+
+const ticket_router = express.Router();
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Ticket:
+ *       type: object
+ *       required:
+ *         - title
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: "The title of the ticket"
+ *       example:
+ *         title: Use swagger for the documentation
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Tickets
+ */
+
+/**
+ * @swagger
+ * /tickets:
+ *   get:
+ *     summary: Return all tickets
+ *     tags: [Tickets]
+ *     responses:
+ *       200:
+ *         description: All stored tickets
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Ticket'
+ */
+ticket_router.get("/", (req, res) => {
+    res.send("No data stored")
+})
+
+export default module.exports = ticket_router
